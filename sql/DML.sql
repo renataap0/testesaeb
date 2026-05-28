65a5c74d5d8c3444422907ccb3a2a4242cd8f650
murilosantos8908 <murilo.d.araujo@aluno.senai.br>
2026-05-28 15:04:02 -0300

    atualiza scripts SQL para criação de banco de dados e tabelas, ajusta inserções e cria view de estoque

--- sql/DML.sql
+++ sql/DML.sql
@@ -1,16 +1,15 @@
 SHOW DATABASES;
-USE renatinhavw;
+USE atividadesaeb;
 
 SHOW TABLES;
 SELECT * FROM produtos;
 
-INSERT INTO produtos (nome, quantidade, valor_unidade, categoria) VALUES
-	("Barra de Chocolate", 52, 8.00, "Alimento"),
-    ("Picanha", 60, 75.00, "Açougue"),
+INSERT INTO produtos (nome, quantidade, valor, categoria) VALUES
+    ("Barra de Chocolate", 52, 8.00, "Alimento"),
+    ("Picanha", 60, 75.00, "Acougue"),
     ("Barrinha de Cereal", 45, 2.00, "Alimento");
-    
 
-INSERT INTO movimentacoes (dt, tipo, quantidade, id_produtos) VALUES
-	("2026-03-25", "Entrada", 15, 1),
-    ("2026-02-14", "Saída", 10, 2),
+INSERT INTO movimentacoes (`date`, tipo, quantidade, produtos_idprodutos) VALUES
+    ("2026-03-25", "Entrada", 15, 1),
+    ("2026-02-14", "Saida", 10, 2),
     ("2026-05-03", "Entrada", 8, 3);
